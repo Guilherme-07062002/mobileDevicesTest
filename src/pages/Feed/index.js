15c236fade5e64193b2943e2947aacdd247260bb
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-06 17:06:42 -0300

    header feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -5,7 +5,27 @@ import styles from "./style";
 export default function Feed({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Feed</Text>
+      <View style={styles.header}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 140,
+            alignItems: "center",
+          }}
+        >
+          <Image source={require("../../../assets/insta-letter.png")} />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <Text style={{ marginRight: 20 }}>S2</Text>
+            <Text>msg</Text>
+          </View>
+        </View>
+      </View>
 
       <StatusBar style="auto" />
     </View>
--- src/pages/Feed/style.js
+++ src/pages/Feed/style.js
@@ -14,25 +14,11 @@ const styles = StyleSheet.create({
     right: 40,
     fontSize: 20,
   },
-  input: {
-    borderWidth: 2,
-    borderRadius: 10,
-    borderColor: "#D1D1D1",
-    backgroundColor: "#fff",
-    color: "#B8B8B8",
-    width: "85%",
-    height: 70,
-    paddingHorizontal: 20,
-    textAlignVertical: "center",
-  },
-  btnEnter: {
-    justifyContent: "center",
-    textAlign: "center",
-    alignItems: "center",
-    backgroundColor: "#1967DB",
-    borderRadius: 30,
-    paddingVertical: 10,
-    width: "80%",
+  header: {
+    position: "absolute",
+    top: "6%",
+    borderStartColor: "red",
+    
   },
 });
 
