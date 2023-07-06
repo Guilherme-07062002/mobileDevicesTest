1f2601f2d3931b8555c48976e7075ecbec48fd84
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-06 16:03:16 -0300

    textInput

--- src/pages/Home.js/index.js
+++ src/pages/Home.js/index.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TextInput } from "react-native";
 import styles from "./style";
 
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Português (Brasil)</Text>
+      <Text style={styles.textStyle}>Português (Brasil)</Text>
       <Image source={require("../../../assets/instagram-icon.png")} />
+
+      <TextInput style={styles.input}></TextInput>
+      <TextInput style={styles.input}></TextInput>
 
       <StatusBar style="auto" />
     </View>
--- src/pages/Home.js/style.js
+++ src/pages/Home.js/style.js
@@ -6,7 +6,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    gap: 60,
+    gap: 50,
+  },
+  inputContainer: {
+    flex: 1,
+    gap: 30,
+  },
+  textStyle: {
+    color: "#919191",
+  },
+  input: {
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "#D1D1D1",
+    backgroundColor: "#fff",
+    color: "#B8B8B8",
+    width: "85%",
+    height: 70,
+    paddingHorizontal: 20,
   },
 });
 
