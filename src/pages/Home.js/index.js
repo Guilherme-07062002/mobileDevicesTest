aeb4ecb178cf37641683b3b3e8a3d8c3287833a8
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-06 15:46:37 -0300

    add instagram home icon

--- src/pages/Home.js/index.js
+++ src/pages/Home.js/index.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import styles from "./style";
 
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>Português (Brasil)</Text>
+      <Image source={require("../../../assets/instagram-icon.png")} />
 
       <StatusBar style="auto" />
     </View>
